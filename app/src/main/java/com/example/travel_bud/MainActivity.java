abab0c4fef94b9d6574bb3487efc7b9c3a42014a
gurleen131 <grlnsethi007@gmail.com>
2024-02-12 23:32:54 +0530

    new

--- app/src/main/java/com/example/travel_bud/MainActivity.java
+++ app/src/main/java/com/example/travel_bud/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.travel_bud;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView tasksRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
